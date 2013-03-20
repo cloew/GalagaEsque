00c361e2d598bd2a3ef740bf0977c0478d84925f
Chris Loew <cloew123@gmail.com>
2013-03-20 16:24:28 -0600

    Added a Game Object Rectangle Class

---
+++ game_object_rectangle.py
@@ -0,0 +1,11 @@
+
+class GameObjectRectangle:
+    """ Represents a rectangle of a Game Object """
+
+    def __init__(self, xSize, ySize):
+        """ Create the Rectangle of the Game Object """
+        self.x = 0
+        self.y = 0
+
+        self.width = xSize
+        self.height = ySize
